434b6d29695ca6a987e75ec30158439c3e8b7b50
RoopeshPatel <rpatel@utilitywarehouse.co.uk>
2018-09-25 12:14:21 +0100

    second commit

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/rpatelUW/stringutil"
+)
 
 func main() {
-	fmt.Println("hello world")
+	fmt.Println(stringutil.Reverse("!oG ,olleH"))
 }
